d8079628fbe9ecf6e05d9201bc07f21b022f5a2a
thodson-usgs <thodson@usgs.gov>
2018-09-27 14:19:15 -0500

    setup pip package

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-trendy: Algorithms for detecting trends in time-series data
+trend: algorithms for detecting trends in time-series data
 ==========================================================
 
-What is trendy?
+What is trend?
 ---------------
-trendy is a python package for detecting trends in time-series data.
+trend is a python package for detecting trends in time-series data.
 It's design and documention borrow heavily from the R package known as
 **trend** developed by Thorsten Pohlert.
 
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup(name='trendy',
-      version='0.1',
-      description='Algorithms for detecting trends in time-series data.',
-      url='https://github.com/USGS-python/trendy',
-      author='Timothy Hodson',
-      author_email='thodson@usgs.gov',
-      license='MIT',
-      packages=['trendy'],
-      zip_safe=False)
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='trend',
+    version='0.1',
+    description='Algorithms for detecting trends in time-series data.',
+    url='https://github.com/USGS-python/trend',
+    author='Timothy Hodson',
+    author_email='thodson@usgs.gov',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
